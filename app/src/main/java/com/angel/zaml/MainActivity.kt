fb12da4505a8e2ad82999e5590df88b691ee56a2
Adrian <adrian@adelgado.net>
2022-01-13 19:18:30 +0100

    Antes de tocar para mirar los tres puntos

--- app/src/main/java/com/angel/zaml/MainActivity.kt
+++ app/src/main/java/com/angel/zaml/MainActivity.kt
@@ -2,10 +2,28 @@ package com.angel.zaml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    //esta funcion crea menu de los tres puntos
+
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_tres_puntos, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val item_selected: Int = item.itemId
+
+        if (item_selected == R.id.item1) {
+            Firebase.auth.signOut()
+            cambiarPantallaLogin()
+        }
+        return super.onOptionsItemSelected(item)
+    }*/
 }
---
+++ app/src/main/java/com/angel/zaml/Splash.kt
@@ -0,0 +1,32 @@
+package com.angel.zaml
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+//import com.google.firebase.auth.FirebaseAuth
+
+class Splash : AppCompatActivity() {
+    private val tiempo : Long =2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+
+        Handler().postDelayed({
+            //Ver si esta inciada la sesion
+            /*var user = FirebaseAuth.getInstance().currentUser
+            if (user!= null){
+                startActivity(Intent(this, App_Registered::class.java))
+            } else{
+                startActivity(Intent(this, MainActivity::class.java))
+            }*/
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },tiempo)
+
+
+    }
+}
